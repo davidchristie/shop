bf9705669747614dcbad199ea0585a78b09d6afa
David Christie <davidchristie.nz@gmail.com>
2023-12-01 08:04:44 +1300

    Run server on port 4000

--- apps/server/test/system/healthcheck.system.test.ts
+++ apps/server/test/system/healthcheck.system.test.ts
@@ -2,7 +2,7 @@ import { describe, expect, it } from "vitest";
 
 describe("healthcheck", () => {
   it(`returns status "ok"`, async () => {
-    const response = await fetch("http://localhost:3000/api/v1/healthcheck");
+    const response = await fetch("http://localhost:4000/api/v1/healthcheck");
     expect(response.status).toBe(200);
     expect(await response.json()).toMatchInlineSnapshot(`
       {
